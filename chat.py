0644284e8d5f04528e8f7dbfcb34d0f343fd47c3
galwort <wthomasgorbett@gmail.com>
2023-08-07 13:36:40 -0400

    Remove initial system dialogue

--- chat.py
+++ chat.py
@@ -19,17 +19,7 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    dialog = [
-        {
-            "role": "system",
-            "content": "You are a helpful, respectful and honest assistant. "
-            "Always answer as helpfully as possible, while being safe. "
-            "Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. "
-            "Please ensure that your responses are socially unbiased and positive in nature. "
-            "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. "
-            "If you don't know the answer to a question, please don't share false information. ",
-        },
-    ]
+    dialog = []
 
     while True:
         print("Start of loop:\n", dialog)
