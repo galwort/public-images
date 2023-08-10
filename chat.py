0ff3d49dda0531487e29a7e612557d0dca920db1
galwort <wthomasgorbett@gmail.com>
2023-08-10 09:15:26 -0400

    Use hugging face

--- chat.py
+++ chat.py
@@ -1,43 +1,5 @@
-from typing import Optional
-import fire
-from llama import Llama
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-
-def main(
-    ckpt_dir: str,
-    tokenizer_path: str,
-    temperature: float = 0,
-    top_p: float = 0.9,
-    max_seq_len: int = 8192,
-    max_batch_size: int = 8,
-    max_gen_len: Optional[int] = None,
-):
-    generator = Llama.build(
-        ckpt_dir=ckpt_dir,
-        tokenizer_path=tokenizer_path,
-        max_seq_len=max_seq_len,
-        max_batch_size=max_batch_size,
-    )
-
-    dialog = []
-
-    while True:
-        user_dialog = {"role": "user", "content": input("User: ")}
-        dialog.append(user_dialog)
-
-        results = generator.chat_completion(
-            [dialog],
-            max_gen_len=max_gen_len,
-            temperature=temperature,
-            top_p=top_p,
-        )
-
-        response = results[0]["generation"]["content"]
-        print("\nLlama:", response)
-        print("\n==================================\n")
-
-        dialog.append({"role": "assistant", "content": response})
-
-
-if __name__ == "__main__":
-    fire.Fire(main)
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
